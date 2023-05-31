9d15a24f3fc582bfd992803e7c5446016f79cbfe
Moises Pires <mouchespires@gmail.com>
2023-05-31 17:28:11 -0300

    add config pm2

---
+++ bitcoin-candle-msg-create/build/enums/CandleColor.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var CandleColor;
+(function (CandleColor) {
+    CandleColor["GREEN"] = "green";
+    CandleColor["RED"] = "red";
+    CandleColor["UNDETERMINED"] = "undetermined";
+})(CandleColor || (CandleColor = {}));
+exports.default = CandleColor;
+//# sourceMappingURL=CandleColor.js.map
